151186f8167fa69e7ee94aeed073f6eb78a2285f
Ryan Dixon <r.dixon@reply.com>
2020-03-25 20:15:26 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,50 +1,79 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from 'react';
+import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import Players from './components/Players';
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+import PlayerSelect from './containers/PlayerSelect';
+import GameBoard from './containers/GameBoard';
 
 class App extends Component {
+  state = {
+    players: [],
+    gameStarted: false,
+    currentPlayer: 1,
+    square: null
+  };
+
+  startGame = (players) => {
+    this.setState({
+      players: players.map(player => ({
+        ...player,
+        location: 0,
+        score: 0
+      })),
+      gameStarted: true
+    });
+  }
+
+  movePlayer = (number, squares) => {
+    this.setState((prevState) => {
+      let currentPlayer = prevState.players.find(player => player.number === prevState.currentPlayer);
+      const location = currentPlayer.location + number;
+      const landingSquare = squares[location % squares.length];
+      const increaseScore = landingSquare.type === currentPlayer.pawn ? 2 : -1;
+
+      currentPlayer = {
+        ...currentPlayer,
+        location: currentPlayer.location + number,
+        score: Math.max(0,  currentPlayer.score + increaseScore)
+      }
+
+      const square = {
+        player: currentPlayer.number,
+        type: landingSquare.type,
+        points: (increaseScore > 0 ? '+' : '') + increaseScore
+      };
+
+      return {
+        square,
+        players: prevState.players.map(player => {
+          if (player.number === prevState.currentPlayer) {
+            return currentPlayer;
+          }
+          return player;
+        }),
+        currentPlayer: prevState.currentPlayer === 1 ? 2 : 1
+      };
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <h1>Front-end-opoly</h1>
+        <Players players={this.state.players} />
+        {
+          this.state.gameStarted ?
+          <GameBoard
+            movePlayer={this.movePlayer}
+            currentPlayer={this.state.currentPlayer}
+            square={this.state.square}
+            players={this.state.players} /> :
+          <PlayerSelect startGame={this.startGame} />
+        }
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
